a1767822451b803ad340e919698b0c2c52b5b8a3
Jay Lo <jaylo@kkstream.com>
2023-09-27 16:07:24 +0800

    Add a sample project called BasicCasting

---
+++ Samples/BasicCasting/BasicCasting/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  BasicCasting
+//
+//  Created by Tsung Cheng Lo on 2023/9/27.
+//
+
+import UIKit
+import KKSPlayer
+import GoogleCast
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Initialize ChromeCast support for this application
+        CastManager.initializeCasting()
+        
+        // Initialize logging
+        GCKLogger.sharedInstance().delegate = self
+        
+        return true
+    }
+}
+
+extension AppDelegate: GCKLoggerDelegate {
+    public func log(fromFunction function: UnsafePointer<Int8>, message: String) {
+        print("ChromeCast Log: \(function) \(message)")
+    }
+}
