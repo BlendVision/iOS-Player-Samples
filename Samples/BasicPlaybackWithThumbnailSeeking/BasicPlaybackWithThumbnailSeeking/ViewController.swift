c728eb28b652d6840442a41bdaa2360350da305c
Sunny Cheng <>
2024-06-27 11:51:07 +0800

    Push player's setting panel and fix invalid url in BasicPlaybackWithThumbnailSeeking.

--- Samples/BasicPlaybackWithThumbnailSeeking/BasicPlaybackWithThumbnailSeeking/ViewController.swift
+++ Samples/BasicPlaybackWithThumbnailSeeking/BasicPlaybackWithThumbnailSeeking/ViewController.swift
@@ -23,11 +23,11 @@ class ViewController: UIViewController {
         self.view.backgroundColor = .black
         
         // Define needed resources
-        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/d65451ac-f080-42c7-b3e2-746c4ca40fa7/vod/hls.m3u8") else {
+        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/ff3cdb1e-558d-4e4c-8219-8234e4a16308/vod/hls.m3u8") else {
             return
         }
         
-        guard let thumbnailUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/d65451ac-f080-42c7-b3e2-746c4ca40fa7/thumbnail/00/thumbnails.vtt") else {
+        guard let thumbnailUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/ff3cdb1e-558d-4e4c-8219-8234e4a16308/thumbnail/00/thumbnails.vtt") else {
             return
         }
         
@@ -46,6 +46,7 @@ class ViewController: UIViewController {
         
         playerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         playerView.frame = view.bounds
+        playerView.add(listener: self)
         
         view.addSubview(playerView)
         view.bringSubviewToFront(playerView)
@@ -71,5 +72,24 @@ extension ViewController: UniPlayerListener {
     func player(_ player: UniPlayer, didReceiveOnEvent event: UniEvent) {
         // Uncomment the following line to observe the event of player
         // debugPrint("event=\(event)")
+    }
+}
+
+extension ViewController: UniUserInterfaceListener {
+    
+    func playerView(_ view: UniPlayerView, didReceiveSettingPressed event: UniUIEvent) {
+
+        if #available(iOS 15.0, *) {
+            let navController = UniSheetPresentationController(
+                rootViewController: UniSettingViewController(player: player)
+            )
+            present(navController, animated: true)
+        } else {
+            // Fallback on earlier versions
+            let navController = UINavigationController(
+                rootViewController: UniSettingViewController(player: player)
+            )
+            present(navController, animated: true)
+        }
     }
 }
