74cdff6a55594c657bef12f0def503f43f777c62
Sunny <sunnycheng@kkcompany.com>
2024-07-01 11:38:33 +0800

    Add BasicPlayerWithPIP sample

--- Samples/BasicPlaybackWithPIP/BasicPlaybackWithPIP/AppDelegate.swift
+++ Samples/BasicPlaybackWithPIP/BasicPlaybackWithPIP/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import AVFAudio
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playback)
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
         return true
     }
 
--- Samples/BasicPlaybackWithPIP/BasicPlaybackWithPIP/ViewController.swift
+++ Samples/BasicPlaybackWithPIP/BasicPlaybackWithPIP/ViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 import BVPlayer
 
+/// To use Picture in Picture (PIP) on iOS, the app needs to be configured in two ways, and users need to enable PIP in their settings.
+/// 1. Add Background Modes and check "Audio, Airplay, and Picture in Picture"
+/// 2. Set up AVAudioSession
+///
+/// More information can be found here: https://developer.apple.com/documentation/avfoundation/media_playback/configuring_your_app_for_media_playback
 class ViewController: UIViewController {
-    
     var player: UniPlayer!
     
     deinit {
@@ -18,11 +22,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .black
         
         // Define needed resources
-        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/d65451ac-f080-42c7-b3e2-746c4ca40fa7/vod/hls.m3u8")else {
+        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/70d2984e-0327-4da9-bb81-9e35f8b7c8a1/vod/hls.m3u8")else {
             return
         }
         
@@ -33,14 +36,20 @@ class ViewController: UIViewController {
         // Create player based on player config
         player = UniPlayerFactory.create(player: playerConfig)
         
+        // Create player view config and enable PIP feature
+        var playerViewConfig = PlayerViewConfig()
+        playerViewConfig.pictureInPictureConfig.isEnabled = true
+       
         // Create player view and pass the player instance to it
-        let playerView = UniPlayerView(player: player, frame: .zero)
+        let playerView = UniPlayerView(player: player, frame: .zero, playerViewConfig: playerViewConfig)
         
         // Listen to player events
         player.add(listener: self)
         
         playerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         playerView.frame = view.bounds
+        
+        // Listen to playerView events
         playerView.add(listener: self)
         
         view.addSubview(playerView)
@@ -63,7 +72,6 @@ extension ViewController: UniPlayerListener {
 extension ViewController: UniUserInterfaceListener {
     
     func playerView(_ view: UniPlayerView, didReceiveSettingPressed event: UniUIEvent) {
-
         if #available(iOS 15.0, *) {
             let navController = UniSheetPresentationController(
                 rootViewController: UniSettingViewController(player: player)
