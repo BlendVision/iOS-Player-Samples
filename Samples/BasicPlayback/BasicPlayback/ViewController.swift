36fb7ced793c5f79ec29479b4f34e62f8d1fe0a8
Sunny <sunnycheng@kkcompany.com>
2024-07-01 16:19:14 +0800

    Add valid content source

--- Samples/BasicPlayback/BasicPlayback/ViewController.swift
+++ Samples/BasicPlayback/BasicPlayback/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         self.view.backgroundColor = .black
         
         // Define needed resources
-        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/d65451ac-f080-42c7-b3e2-746c4ca40fa7/vod/hls.m3u8")else {
+        guard let streamUrl = URL(string: "https://d1kn28obgh8dky.cloudfront.net/7d1e6d5e-6245-43a3-aba0-8e16cf353db5/vod/70d2984e-0327-4da9-bb81-9e35f8b7c8a1/vod/hls.m3u8")else {
             return
         }
         
