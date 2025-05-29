9983dc19f76b49ebf842aed1026a8070bc48ec3c
Jay Lo <jaylo@kkstream.com>
2025-05-29 16:34:44 +0800

    chore: update sample code to match BVPlayer 5.0.0 API usage

--- Samples/BasicAudioPlayback/BasicAudioPlayback/ViewController.swift
+++ Samples/BasicAudioPlayback/BasicAudioPlayback/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         playerConfig.playbackConfig.isAutoplayEnabled = false
 
         // Create player based on player config
-        player = UniPlayerFactory.create(player: playerConfig)
+        player = UniPlayerFactory.createPlayer(playerConfig: playerConfig)
         
         // Create player view and pass the player instance to it
         playerView = UniPlayerView(player: player, frame: .zero)
